740eebfc244a4241cb811c9fef47b8d6db03d0db
Admin <thubichvi98@gmail.com>
2022-11-28 11:31:11 +0700

    edit ArrayListLearning

--- src/Lession9/ArrayListLearning.java
+++ src/Lession9/ArrayListLearning.java
@@ -1,6 +1,9 @@
 package Lession9;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ArrayListLearning {
     public static void main(String[] args) {
@@ -8,7 +11,41 @@ public class ArrayListLearning {
         ArrayList<Integer> myNumList = new ArrayList<Integer>();
         ArrayList<String> myStringList = new ArrayList<String>();
 
+        Float [] floatNumArray = {1.2f, 1.3f, 1.4f};
+        List<Float> myFloatList = Arrays.asList(floatNumArray);
+
+        //2. Add insert Edit Delete element in ArrayList
+        //add random integer in the last position of list
+        SecureRandom secureRandom = new SecureRandom();
+        for(int i = 0; i <= 10; i++){
+            myNumList.add(secureRandom.nextInt());
+        }
+
+        //add random integer in somewhere of List
+        myNumList.add(2, 1000);
+
+        //delete value
+        myNumList.remove(0);
+
+        //edit value
+        myNumList.remove(0);
+        myNumList.add(0, 100);
+
+        //3. Interating ArrayList, normal for and for-each loop
+        for(int i = 0; i< myNumList.size(); i++){
+            System.out.println(myNumList.get(i));
+        }
+        System.out.println("============");
+
+        for(int i : myNumList)
+        {
+            System.out.println(i);
+        }
+        System.out.println("============");
+
+        //4. Check size, isEmpty
+        System.out.println("Is myNumList empty: " + myNumList.isEmpty());
+        System.out.println("Is myStringList empty: " + myStringList.isEmpty());
+
     }
-
-
 }
